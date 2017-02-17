b5deb63a559b555f0573ddc91f15c80cb77bcce6
devesh <deveshasha.2@gmail.com>
2017-02-17 23:21:49 +0530

    Image passing via uri for capture/gallery

--- app/src/main/java/com/beproj/camcoder/MainActivity.java
+++ app/src/main/java/com/beproj/camcoder/MainActivity.java
@@ -1,10 +1,7 @@
 package com.beproj.camcoder;
 
-import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -12,14 +9,8 @@ import android.provider.MediaStore;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.Toast;
 
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
@@ -28,8 +19,7 @@ import java.util.Date;
 public class MainActivity extends AppCompatActivity {
 
     private int REQUEST_CAMERA = 0, SELECT_FILE = 1;
-    private String mImageFileLocation = "";
-    private File image;
+    private Uri photoFileUri;
 
 
     @Override
@@ -38,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(com.beproj.camcoder.R.layout.activity_main);
     }
 
-    public void takePhoto(View view) {
+    public void takePhotoIntent(View view) {
         Intent callCameraApplicationIntent = new Intent();
         callCameraApplicationIntent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
 
@@ -49,29 +39,15 @@ public class MainActivity extends AppCompatActivity {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        callCameraApplicationIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile));
+
+        photoFileUri = Uri.fromFile(photoFile);
+        callCameraApplicationIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoFileUri);
+
         startActivityForResult(callCameraApplicationIntent, REQUEST_CAMERA);
     }
 
-    protected void onActivityResult (int requestCode, int resultCode, Intent data) {
-        if(requestCode == REQUEST_CAMERA && resultCode == RESULT_OK) {
-            Intent open_displayPage=new Intent(this,showPreview.class);
-            open_displayPage.putExtra("code",0);
-            open_displayPage.putExtra("imagePath", mImageFileLocation);
-            startActivity(open_displayPage);
-        }
-        else if(requestCode == SELECT_FILE && resultCode == RESULT_OK){
-            onSelectFromGalleryResult(data);
-        }
-    }
-
     File createImageFile() throws IOException {
-
-        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
-                Environment.DIRECTORY_PICTURES), "Camcoder");
-        // This location works best if you want the created images to be shared
-        // between applications and persist after your app has been uninstalled.
-
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "Camcoder");
         // Create the storage directory if it does not exist
         if (! mediaStorageDir.exists()){
             if (! mediaStorageDir.mkdirs()){
@@ -79,57 +55,36 @@ public class MainActivity extends AppCompatActivity {
                 return null;
             }
         }
-
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = "IMAGE_" + timeStamp + "_";
-
-        //File storageDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
-        //image = File.createTempFile(imageFileName,".jpg", mediaStorageDir);
-
-        image = new File(mediaStorageDir.getPath() + File.separator + "IMG_"+ timeStamp + ".jpg");
-        mImageFileLocation = image.getAbsolutePath();
-
+        File image = new File(mediaStorageDir.getPath() + File.separator + "IMG_"+ timeStamp + ".jpg");
         return image;
-
     }
 
     public void galleryIntent(View view)    {
-        Intent intent = new Intent();
-        intent.setType("image/*");
-        intent.setAction(Intent.ACTION_GET_CONTENT);//
-        startActivityForResult(Intent.createChooser(intent, "Select File"),SELECT_FILE);
+        Intent getIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        getIntent.setType("image/*");
+
+        Intent pickIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        pickIntent.setType("image/*");
+
+        Intent chooserIntent = Intent.createChooser(getIntent, "Select Image");
+        chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Intent[] {pickIntent});
+
+        startActivityForResult(chooserIntent, SELECT_FILE);
     }
 
-    @SuppressWarnings("deprecation")
-    private void onSelectFromGalleryResult(Intent data) {
-
-        /*String path = data.getData().getPath();
-        Intent open_displayPage = new Intent(this,showPreview.class);
-        open_displayPage.putExtra("code",1);
-        open_displayPage.putExtra("imageGalleryPath", path);
-        startActivity(open_displayPage);*/
-
-        Bitmap bm = null;
-        if (data != null) {
-            try {
-                bm = MediaStore.Images.Media.getBitmap(getApplicationContext().getContentResolver(), data.getData());
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+    protected void onActivityResult (int requestCode, int resultCode, Intent data) {
+        if(requestCode == REQUEST_CAMERA && resultCode == RESULT_OK) {
+            Intent open_displayPage = new Intent(this,showPreview.class);
+            open_displayPage.putExtra("imageUri",photoFileUri.toString());
+            startActivity(open_displayPage);
         }
-        Intent open_displayPage = new Intent(this,showPreview.class);
-        open_displayPage.putExtra("code",1);
-
-        ByteArrayOutputStream bs = new ByteArrayOutputStream();
-        bm.compress(Bitmap.CompressFormat.JPEG, 50, bs);
-        open_displayPage.putExtra("byteArray", bs.toByteArray());
-        startActivity(open_displayPage);
-
-
-        open_displayPage.putExtra("galleryImage", bm);
-        startActivity(open_displayPage);
-
-        //ivImage.setImageBitmap(bm);
+        else if(requestCode == SELECT_FILE && resultCode == RESULT_OK){
+            Uri fileuri = data.getData();
+            Intent open_displayPage = new Intent(this,showPreview.class);
+            open_displayPage.putExtra("imageUri",fileuri.toString());
+            startActivity(open_displayPage);
+        }
     }
 
     @Override
--- app/src/main/java/com/beproj/camcoder/showPreview.java
+++ app/src/main/java/com/beproj/camcoder/showPreview.java
@@ -1,22 +1,12 @@
 package com.beproj.camcoder;
 
-/*import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
-public class showPreview extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_show_preview);
-    }
-}*/
-
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
+import android.provider.MediaStore;
 import android.util.Base64;
 import android.view.View;
 import android.widget.Button;
@@ -32,71 +22,33 @@ import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class showPreview extends Activity {
-    private static final String TAG = "value";
+    //private static final String TAG = "value";
     private ImageView mPhotoCapturedImageView;
-    private String mImageFileLocation = "",image_name="";
-    private Bitmap b, photoReducedSizeBitmp;
-    private int code;
+    private String image_name="";
+    private Bitmap bitmap;
     private String encoded_string;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
 
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_preview);
 
-        /*Intent i = getIntent();
-        Bitmap photoReducedSizeBitmp = (Bitmap) i.getParcelableExtra("bmp_Image");
-        mPhotoCapturedImageView = (ImageView) findViewById(R.id.capturePhotoImageView);
-        mPhotoCapturedImageView.setImageBitmap(photoReducedSizeBitmp);*/
-        code = getIntent().getIntExtra("code", -1);
-        mImageFileLocation = getIntent().getStringExtra("imagePath");
-        //mImageGalleryFileLocation = getIntent().getStringExtra("imageGalleryPath");
         mPhotoCapturedImageView = (ImageView) findViewById(R.id.capturePhotoImageView);
 
-        // Bitmap bitmap = (Bitmap) getIntent().getParcelableExtra("galleryImage");
-
-
-        if (code == 1) {
-            b = BitmapFactory.decodeByteArray(
-                    getIntent().getByteArrayExtra("byteArray"), 0, getIntent()
-                            .getByteArrayExtra("byteArray").length);
-
-            mPhotoCapturedImageView.setImageBitmap(b);
-
-        } else if (code == 0)
-            showReducedSize(mImageFileLocation);
-        else
-            Toast.makeText(getApplicationContext(), "Cannot load. Error code -1", Toast.LENGTH_SHORT);
-        //showReducedSize(String mImageGalleryFileLocation );
-
+        Uri imageUri = Uri.parse(getIntent().getStringExtra("imageUri"));
+        try {
+            bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        mPhotoCapturedImageView.setImageBitmap(bitmap);
     }
-
-    public void showReducedSize(String path) {
-        int targetImageViewWidth = 420; //mPhotoCapturedImageView.getWidth();
-        int targetImageViewHeight = 420; //mPhotoCapturedImageView.getHeight();
-
-        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
-        bmOptions.inJustDecodeBounds = true;
-        BitmapFactory.decodeFile(path, bmOptions);
-        int cameraImageWidth = bmOptions.outWidth;
-        int cameraImageHeight = bmOptions.outHeight;
-
-        int scaleFactor = Math.min(cameraImageWidth / targetImageViewWidth, cameraImageHeight / targetImageViewHeight);
-        bmOptions.inSampleSize = scaleFactor;
-        bmOptions.inJustDecodeBounds = false;
-
-        photoReducedSizeBitmp = BitmapFactory.decodeFile(path, bmOptions);
-        /*Intent intent = new Intent(this, showPreview.class);
-        intent.putExtra("bmp_Image", photoReducedSizeBitmp);
-        startActivity(intent);*/
-        mPhotoCapturedImageView.setImageBitmap(photoReducedSizeBitmp);
-
-    }
-
 
     public void uploadFunc(View view) {
         new Encode_image().execute();
@@ -105,24 +57,12 @@ public class showPreview extends Activity {
     private class Encode_image extends AsyncTask<Void, Void, Void> {
         @Override
         protected Void doInBackground(Void... voids) {
-                ByteArrayOutputStream stream = new ByteArrayOutputStream();
-                if (code == 1) {
-                    // bitmap = BitmapFactory.decodeFile(file_uri.getPath());
-                    b.compress(Bitmap.CompressFormat.JPEG, 100, stream);
-                  //  b.recycle();
 
-                    byte[] array = stream.toByteArray();
-                    encoded_string = Base64.encodeToString(array, 0);
-                    return null;
-                } else if (code == 0) {
-                    photoReducedSizeBitmp.compress(Bitmap.CompressFormat.JPEG, 100, stream);
-                  //  photoReducedSizeBitmp.recycle();
-
-                    byte[] array = stream.toByteArray();
-                    encoded_string = Base64.encodeToString(array, 0);
-                    return null;
-
-                }
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.JPEG,100,stream);
+            bitmap.recycle();
+            byte[] array = stream.toByteArray();
+            encoded_string = Base64.encodeToString(array, 0);
             return null;
         }
 
