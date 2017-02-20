987ef1a0f1708684b8e8560bf742c54eca2d766f
nikhil <nikuu13@gmail.com>
2017-02-20 13:18:51 +0530

    unfixed upload

--- app/src/main/java/com/beproj/camcoder/showPreview.java
+++ app/src/main/java/com/beproj/camcoder/showPreview.java
@@ -1,14 +1,20 @@
 package com.beproj.camcoder;
 
+import android.app.ProgressDialog;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
@@ -18,9 +24,20 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
+
+import static com.android.volley.VolleyLog.TAG;
 
 public class showPreview extends Activity {
 
@@ -29,6 +46,7 @@ public class showPreview extends Activity {
     private String encoded_string;
     private int code;
     private Bitmap bitmap;
+    Uri imageuri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,18 +56,17 @@ public class showPreview extends Activity {
 
         mPhotoCapturedImageView = (ImageView) findViewById(R.id.capturePhotoImageView);
         code = getIntent().getIntExtra("code", -1);
-
-        if(code == 0){
-            Uri imageuri = Uri.parse(getIntent().getStringExtra("imagePath"));
-            String path = imageuri.getPath();
+        imageuri = Uri.parse(getIntent().getStringExtra("imagePath"));
+        image_name = getfilename(imageuri);
+        String path = imageuri.getPath();
+       /* if(code == 0){
             showReducedSize(path);
         }
         else if(code == 1){
-            Uri imageuri = Uri.parse(getIntent().getStringExtra("imagePath"));
-            String imagepath = imageuri.getPath();
             showReducedSize(imagepath);
 
-        }
+        }*/
+        showReducedSize(path);
     }
 
     public void showReducedSize(String path) {
@@ -72,16 +89,125 @@ public class showPreview extends Activity {
     }
 
     public void uploadFunc(View view) {
-        new Encode_image().execute();
+        ServerTask task = new ServerTask();
+        task.execute("");
+      //  task.execute( Environment.getExternalStorageDirectory().toString() +image_name);
+        /*URL url = null;
+        String ans="";
+        try {
+            url = new URL("http://192.168.56.1/camcoder/recognize.php");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        HttpURLConnection urlConnection=null;
+        try {
+            urlConnection = (HttpURLConnection) url.openConnection();
+            InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+            ans = readStream(in);
+        }
+        catch (MalformedURLException ex){
+            Log.e(TAG, "error: " + ex.getMessage(), ex);
+
+        }
+
+
+
+        finally {
+            urlConnection.disconnect();
+        }
+
+        TextView t = (TextView) findViewById(R.id.textView);
+        t.setText(ans);*/
     }
 
-    private class Encode_image extends AsyncTask<Void, Void, Void> {
+    public class ServerTask  extends AsyncTask<String, Integer , Void> {
+
+        URL url = null;
+        String ans="";
+        public void process() {
+
+            try {
+                url = new URL("http://camcoder/recognize.php");
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+            HttpURLConnection urlConnection = null;
+            try {
+                urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+                ans = readStream(in);
+            } catch (MalformedURLException ex) {
+                Log.e(TAG, "error: " + ex.getMessage(), ex);
+
+            } catch (IOException ex) {
+                Log.e(TAG, "error: " + ex.getMessage(), ex);
+
+            }
+
+            finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+            }
+
+            /*TextView t = (TextView) findViewById(R.id.textView);
+            t.setText(ans);*/
+        }
+        @Override
+        protected void onPostExecute(Void param) {
+            TextView t = (TextView) findViewById(R.id.textView);
+            t.setText(ans);
+        }
+
+
+
+        private String readStream(InputStream is) {
+
+            BufferedReader br = null;
+            StringBuilder sb = new StringBuilder();
+
+            String line;
+            try {
+
+                br = new BufferedReader(new InputStreamReader(is));
+                while ((line = br.readLine()) != null) {
+                    sb.append(line);
+                    sb.append("\n\r");
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (br != null) {
+                    try {
+                        br.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            return sb.toString();
+
+        }
+
+        @Override
+        protected Void doInBackground(String... strings) {
+            process();
+            return null;
+        }
+    }
+
+
+    /*private class Encode_image extends AsyncTask<Void, Void, Void> {
         @Override
         protected Void doInBackground(Void... voids) {
 
+            bitmap = BitmapFactory.decodeFile(imageuri.getPath());
             ByteArrayOutputStream stream = new ByteArrayOutputStream();
-            bitmap.compress(Bitmap.CompressFormat.JPEG,100,stream);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
             bitmap.recycle();
+
             byte[] array = stream.toByteArray();
             encoded_string = Base64.encodeToString(array, 0);
             return null;
@@ -91,22 +217,49 @@ public class showPreview extends Activity {
         protected void onPostExecute(Void aVoid) {
             makeRequest();
         }
+    }*/
+    public String getfilename(Uri uri){
+        if (uri.getScheme().equals("file")) {
+            image_name = uri.getLastPathSegment();
+        } else {
+            Cursor cursor = null;
+            try {
+                cursor = getContentResolver().query(uri, new String[]{
+                        MediaStore.Images.ImageColumns.DISPLAY_NAME
+                }, null, null, null);
+
+                if (cursor != null && cursor.moveToFirst()) {
+                    image_name = cursor.getString(cursor.getColumnIndex(MediaStore.Images.ImageColumns.DISPLAY_NAME));
+                    Log.d(TAG,"name is " + image_name);
+                }
+            } finally {
+
+                if (cursor != null) {
+                    cursor.close();
+                }
+            }
+        }
+        return image_name;
     }
 
-    private void makeRequest() {
+
+
+    /*private void makeRequest() {
         RequestQueue requestQueue = Volley.newRequestQueue(this);
-        StringRequest request = new StringRequest(Request.Method.POST, "http://192.168.56.1/camcoder/recognize.php",
+        StringRequest request = new StringRequest(Request.Method.GET, "http://192.168.56.1/camcoder/recognize.php",
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
-
+                        TextView t = (TextView) findViewById(R.id.textView);
+                        t.setText(response.substring(0,500));
                     }
                 }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-
+                TextView t = (TextView) findViewById(R.id.textView);
+                t.setText("Didnt work");
             }
-        }) {
+        }); *//*{
             @Override
             protected Map<String, String> getParams() throws AuthFailureError {
                 HashMap<String,String> map = new HashMap<>();
@@ -115,7 +268,7 @@ public class showPreview extends Activity {
 
                 return map;
             }
-        };
+        };*//*
         requestQueue.add(request);
-    }
+    }*/
 }
